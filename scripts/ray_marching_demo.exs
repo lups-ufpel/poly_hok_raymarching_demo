15953eb496d93f86c887632ea873006d3848ac9c
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-09-19 23:48:06 -0300

    Made FPS configurable via command line

--- scripts/ray_marching_demo.exs
+++ scripts/ray_marching_demo.exs
@@ -3,5 +3,13 @@ use RayMarching
 window_x = 500
 window_y = 600
 
+argv = System.argv()
+fps =
+  if length(argv) >= 1 do
+    String.to_integer(hd(argv))
+  else
+    60
+  end
+
 SDL2.start("Ray Marching Demo", window_x, window_y)
-RayMarching.start_render(window_x, window_y, 60)
+RayMarching.start_render(window_x, window_y, fps)
